052787ab749da20426b945209633668eae746f05
Ishan Tiwari <poet.infamous@gmail.com>
2022-04-12 19:52:54 +0530

    Added python decouple

--- backend/stacksonfirebackend/stacksonfirebackend/settings.py
+++ backend/stacksonfirebackend/stacksonfirebackend/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import dj_database_url
+from python-decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
