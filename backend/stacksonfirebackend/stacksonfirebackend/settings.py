eb431a15e263c791857ca1fde85069dea46e674b
Ishan Tiwari <poet.infamous@gmail.com>
2022-04-13 18:26:36 +0530

    Correct static_files variable

--- backend/stacksonfirebackend/stacksonfirebackend/settings.py
+++ backend/stacksonfirebackend/stacksonfirebackend/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT='/staticfiles/'
+STATIC_ROOT=BASE_DIR + '/staticflies/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
