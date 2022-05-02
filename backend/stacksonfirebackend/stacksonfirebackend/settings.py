f57d406cf3148aed2dd85b4e83c1ae4d92d0bce5
Ishan Tiwari <poet.infamous@gmail.com>
2022-05-02 10:25:33 +0000

    Updated password

--- backend/stacksonfirebackend/stacksonfirebackend/settings.py
+++ backend/stacksonfirebackend/stacksonfirebackend/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'stacksonfirebackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['PGDATABASE'],
+        'USER':os.environ['PGUSER'],
+        'PASSWORD':os.environ['PGPASSWORD'],
+        'HOST':os.environ['PGHOST'],
+        'PORT':os.environ['PGPORT'],
     }
 }
 
