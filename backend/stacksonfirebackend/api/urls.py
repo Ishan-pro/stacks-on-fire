b62bbaf273f63e90015b906d8590b724d45b2aed
Ishan Tiwari <poet.infamous@gmail.com>
2022-05-26 07:34:41 +0530

    Corrected File plaement

--- backend/stacksonfirebackend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from api.views import PostViewset, UserViewSet
-from rest_framework import routers
-
-router = routers.SimpleRouter()
-
-router.register('posts', PostViewset)
-router.register('user', UserViewSet)
-urlpatterns = router.urls
--- backend/stacksonfirebackend/posts/serializer.py
+++
@@ -1,9 +0,0 @@
-from .models import Post
-from rest_framework import serializers
-from api.userSerializer import UserSerializer
-
-class PostSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = ['id', 'text', ]
